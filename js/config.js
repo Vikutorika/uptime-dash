afef69f392441b31b6876201a8d1b856cd21cbdb
Pesy Wu <admin@bili33.top>
2021-08-01 17:14:12 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+//array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+		'u903555-a41d2e4ebd886b27d8541a80' //TODO set your API
+	];
+
+//refresh interval (in seconds)
+var __refresh = 300; 
