8b6533a577c662b426c03c9f452892441375a08e
Pesy Wu <admin@bili33.top>
2021-08-01 17:20:25 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ myApp.dashboard = (function($) {
 	/* load uptime variables from uptimerobot
 	*/
 	function getUptime(apikey) {
-		var url = "https://api.uptimerobot.com/v2/getMonitors";
+		var url = "https://uptimerobot.api.bili33.top/v2/getMonitors";
 	
 		$.post(url, {
 			"api_key": apikey,
